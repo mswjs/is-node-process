3023acd74b65a2e5bf9ff67d51f2d4d483514169
Artem Zakharchenko <kettanaito@gmail.com>
2023-02-18 15:51:08 +0100

    chore: add electron app debugging

--- test/electron.test.ts
+++ test/electron.test.ts
@@ -1,23 +1,42 @@
-import * as path from 'path'
 import { ElectronApplication, Page, _electron as electron } from 'playwright'
 
-jest.setTimeout(10000)
+jest.setTimeout(10_000)
 
-let app: ElectronApplication
-let page: Page
+let app: ElectronApplication | undefined
+let page: Page | undefined
 
 beforeAll(async () => {
-  app = await electron.launch({
-    args: [path.resolve(__dirname, 'fixtures/electron.js')],
+  app = await electron
+    .launch({
+      args: [require.resolve('./fixtures/electron.js')],
+    })
+    .catch((error) => {
+      console.error('Failed to launch Electron app', error)
+      return undefined
+    })
+
+  if (!app) {
+    throw new Error('Failed to launch Electron app')
+  }
+
+  page = await app.firstWindow().catch((error) => {
+    console.error('Failed to get first window', error)
+    return undefined
   })
-  page = await app.firstWindow();
 })
 
 afterAll(async () => {
+  if (!app) {
+    console.log({ app, page })
+    throw new Error(
+      'Failed to clean up after test: the Electron app is not defined'
+    )
+  }
+
   await app.close()
 })
 
 it('returns false when run in the Electron renderer process', async () => {
-  const body = await page.locator('body').textContent()
-  expect(body.trim()).toEqual('false')
+  const body = await page!.locator('body').textContent()
+  expect(body!.trim()).toEqual('false')
 })
